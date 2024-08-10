985bdcdf6a2b5ab806b915ff7883ce32ec40e4c9
lx10ng <lx10ng@qq.com>
2024-08-10 14:29:21 +0800

    add nvim config

---
+++ .config/nvim/lua/community.lua
@@ -0,0 +1,17 @@
+-- AstroCommunity: import any community modules here
+-- We import this file in `lazy_setup.lua` before the `plugins/` folder.
+-- This guarantees that the specs are processed before any user plugins.
+
+---@type LazySpec
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.cpp" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.verilog" },
+
+  { import = "astrocommunity.completion.cmp-cmdline" },
+
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  -- import/override with your plugins folder
+}
