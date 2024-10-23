f511537ad55d105053515c5b49406bd2d96ad6ec
lx10ng <lx10ng@qq.com>
2024-10-23 21:22:11 +0800

    add rust pack

--- .config/nvim/lua/community.lua
+++ .config/nvim/lua/community.lua
@@ -7,6 +7,7 @@ return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.cpp" },
+  { import = "astrocommunity.pack.rust" },
   { import = "astrocommunity.pack.python" },
   { import = "astrocommunity.pack.verilog" },
 
