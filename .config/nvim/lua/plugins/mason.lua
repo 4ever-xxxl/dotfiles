44e2a4339c8e87e7daa0e707a8ccce09c1212593
lx10ng <l3ixiong@outlook.com>
2026-01-29 21:44:13 +0800

    更新 Neovim 插件配置，移除 codelldb，禁用 none-ls 以避免与 LSP 服务器冲突，调整 Treesitter 文件大小限制

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -32,7 +32,6 @@ return {
 
         -- rust
         "rust-analyzer",
-        "codelldb",
       },
     },
   },
--- .config/nvim/lua/plugins/none-ls.lua
+++ .config/nvim/lua/plugins/none-ls.lua
@@ -1,13 +1,8 @@
--- Customize None-ls sources
+-- None-ls is completely disabled to avoid conflicts with LSP servers
+-- Using basedpyright for Python diagnostics and ruff_format for formatting instead
 
 ---@type LazySpec
 return {
   "nvimtools/none-ls.nvim",
-  -- Disable none-ls completely to avoid conflicts with LSP servers
-  -- Use basedpyright for Python diagnostics and ruff_format for formatting
   enabled = false,
-  opts = function(_, opts)
-    local null_ls = require "null-ls"
-    opts.sources = require("astrocore").list_insert_unique(opts.sources, {})
-  end,
 }
