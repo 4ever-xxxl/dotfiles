3d68d496a4d2b2df98a7b4b89f085012223aeb94
lx10ng <l3ixiong@outlook.com>
2026-06-11 16:14:58 +0800

    refactor(nvim): replace deprecated vim.loop with vim.uv

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -25,7 +25,7 @@ return {
       disable = function(lang, buf)
         -- Disable highlight for large files to avoid performance issues
         local max_filesize = 500 * 1024 -- 500 KB
-        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+        local ok, stats = pcall(vim.uv.fs_stat, vim.api.nvim_buf_get_name(buf))
         if ok and stats and stats.size > max_filesize then
           return true
         end
